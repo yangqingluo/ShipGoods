b95994a65ccb9399ddfb8313a4b7345731777d68
yangqingluo <84807330@qq.com>
2018-06-12 10:46:03 +0800

    空船cell自适应高度及文本多行显示右对齐调整

--- app/pages/Home/HomeGoodsCell.js
+++ app/pages/Home/HomeGoodsCell.js
@@ -50,46 +50,46 @@ export default class HomeGoodsCell extends Component {
             <View style={{opacity: isOrdered ? 0.5 : 1.0, padding: 10}}>
                 <TouchableHighlight style={styles.cellContainer} onPress={() => this.props.onPress(info)}>
                     <View style={{flex: 1, backgroundColor:'white'}}>
-                        <View style={{backgroundColor:'#81c6ff', flexDirection: 'row', justifyContent: "space-between", height:px2dp(26)}}>
+                        <View style={{backgroundColor:'#81c6ff', flexDirection: 'row', justifyContent: "space-between", height:26}}>
                             <Text style={{fontSize:px2dp(10), color:'white', marginLeft:px2dp(3), marginTop:px2dp(8)}}>{'发票编号：' + info.item.billing_sn}</Text>
                             <Text style={{fontSize:px2dp(10), color:'white', marginRight:px2dp(6), marginTop:px2dp(8)}}>{info.item.create_timetext}</Text>
                         </View>
-                        <View style={{backgroundColor:'#f2f9ff', flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:px2dp(51)}}>
+                        <View style={{backgroundColor:'#f2f9ff', flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:51}}>
                             <Text style={{fontSize:px2dp(14), color:appData.appTextColor, marginLeft:px2dp(28), fontWeight:'bold'}}>{info.item.empty_port_name + ' / ' + info.item.ship_name}</Text>
                             <Text style={{fontSize:px2dp(14), color:appData.appBlueColor, marginRight:px2dp(18), fontWeight:'bold'}}>{info.item.tonnage + ' T'}</Text>
                         </View>
-                        <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:px2dp(47)}}>
-                            <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor, marginLeft:px2dp(28)}}>空船期</Text>
-                            <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor, marginRight:px2dp(18)}}>{info.item.empty_timetext}</Text>
+                        <View style={styles.cellItemContainer}>
+                            <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor}}>空船期</Text>
+                            <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor}}>{info.item.empty_timetext}</Text>
                         </View>
                         {downloadOilList.length > 0 ?
                             <View>
-                                <View style={{height:px2dp(1), marginLeft:px2dp(3), backgroundColor:appData.appSeparatorLightColor}} />
-                                <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:px2dp(47)}}>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor, marginLeft:px2dp(28)}}>下载可运油品</Text>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor, marginRight:px2dp(18)}}>{downloadOilList.join(' ')}</Text>
+                                <View style={styles.cellSeparator}/>
+                                <View style={styles.cellItemContainer}>
+                                    <Text style={{fontSize:14, color:appData.appSecondaryTextColor}}>下载可运油品</Text>
+                                    <Text style={styles.cellItemRightText}>{downloadOilList.join(' ')}</Text>
                                 </View>
                             </View>
                             :null}
                         {uploadOilList.length > 0 ?
                             <View>
-                                <View style={{height:px2dp(1), marginLeft:px2dp(3), backgroundColor:appData.appSeparatorLightColor}} />
-                                <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:px2dp(47)}}>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor, marginLeft:px2dp(28)}}>上载油品</Text>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor, marginRight:px2dp(18)}}>{uploadOilList.join(' ')}</Text>
+                                <View style={styles.cellSeparator}/>
+                                <View style={styles.cellItemContainer}>
+                                    <Text style={{fontSize:14, color:appData.appSecondaryTextColor}}>上载油品</Text>
+                                    <Text style={styles.cellItemRightText}>{uploadOilList.join(' ')}</Text>
                                 </View>
                             </View>
                             :null}
                         <View>
-                            <View style={{height:px2dp(1), marginLeft:px2dp(3), backgroundColor:appData.appSeparatorLightColor}} />
-                            <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "space-between", height:px2dp(47)}}>
+                            <View style={styles.cellSeparator}/>
+                            <View style={styles.cellItemContainer}>
                                 <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "flex-start"}}>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor, marginLeft:px2dp(28)}}>{'可运柴油 '}</Text>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor}}>{info.item.dieseloil + ' T'}</Text>
+                                    <Text style={{fontSize:14, color:appData.appSecondaryTextColor}}>{'可运柴油 '}</Text>
+                                    <Text style={{fontSize:14, color:appData.appLightTextColor}}>{info.item.dieseloil + ' T'}</Text>
                                 </View>
                                 <View style={{flexDirection: 'row',  alignItems: "center", justifyContent: "flex-end"}}>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appSecondaryTextColor}}>{'可运汽油 '}</Text>
-                                    <Text style={{fontSize:px2dp(14), color:appData.appLightTextColor, marginRight:px2dp(18)}}>{info.item.gasoline + ' T'}</Text>
+                                    <Text style={{fontSize:14, color:appData.appSecondaryTextColor}}>{'可运汽油 '}</Text>
+                                    <Text style={{fontSize:14, color:appData.appLightTextColor, textAlign: "right"}}>{info.item.gasoline + ' T'}</Text>
                                 </View>
                             </View>
                         </View>
@@ -115,14 +115,26 @@ const styles = StyleSheet.create({
         borderColor: appData.appBorderColor,
         borderWidth: 0.5,
     },
-
-    // container: {
-    //     marginBottom: 10,
-    //     flex: 1,
-    //     height: 120,
-    //     padding: 10,
-    //     borderRadius: 10,
-    //     backgroundColor: '#D3D3D3',
-    // },
+    cellSeparator: {
+        height:1,
+        marginLeft:3,
+        backgroundColor:appData.appSeparatorLightColor,
+    },
+    cellItemContainer: {
+        flexDirection: 'row',
+        alignItems: "center",
+        justifyContent: "space-between",
+        minHeight:47,
+        paddingRight: 18,
+        paddingLeft: 28,
+        paddingVertical: 5,
+    },
+    cellItemRightText: {
+        flex:1,
+        fontSize:14,
+        color:appData.appLightTextColor,
+        marginLeft:5,
+        textAlign: "right",
+    },
 });
 
