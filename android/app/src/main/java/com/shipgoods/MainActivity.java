62b25c9150333b4aee96839d5c88ece1ad9a43b5
yangqingluo <84807330@qq.com>
2018-06-18 09:50:39 +0800

    安卓6.0动态权限申请实现

--- android/app/src/main/java/com/shipgoods/MainActivity.java
+++ android/app/src/main/java/com/shipgoods/MainActivity.java
@@ -1,8 +1,14 @@
 package com.shipgoods;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
+import android.Manifest;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.text.TextUtils;
+import android.widget.Toast;
 import com.facebook.react.ReactActivity;
 import com.umeng.socialize.UMShareAPI;
 import com.shipgoods.invokenative.ShareModule;
@@ -12,6 +18,21 @@ public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= 23){
+            String[] mPermissionList = new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CALL_PHONE,
+                    Manifest.permission.READ_LOGS,
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.SET_DEBUG_APP,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.GET_ACCOUNTS,
+                    Manifest.permission.WRITE_APN_SETTINGS
+            };
+            ActivityCompat.requestPermissions(this, mPermissionList, 123);
+        }
         ShareModule.initSocialSDK(this);
     }
 
@@ -28,5 +49,19 @@ public class MainActivity extends ReactActivity {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 123) {
+            for (int i = 0; i < permissions.length; i++) {
+                if (grantResults[i] == PERMISSION_GRANTED) {
+//                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请成功", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "" + "权限" + permissions[i] + "申请失败", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
     }
 }
