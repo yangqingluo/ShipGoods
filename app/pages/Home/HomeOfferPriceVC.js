7b1cafa9371aed3358c196ce84f7a89272c98356
yangqingluo <84807330@qq.com>
2018-06-22 11:22:04 +0800

    认同报价界面逻辑进一步调整

--- app/pages/Home/HomeOfferPriceVC.js
+++ app/pages/Home/HomeOfferPriceVC.js
@@ -37,26 +37,36 @@ export default class HomeOfferPriceVC extends Component {
             lastGoodsSelectedList: [],
         };
 
-        this.config = this.isAgreePrice() ?
-            [
+        if (this.isAgreePrice()) {
+            this.config = [
                 {idKey:"ship_name", name:"船舶信息", logo:require('../../images/icon_blue.png'), disable:false, hideArrowForward:true},
                 {idKey:"offer", name:"运价", logo:require('../../images/icon_red.png'), disable:false, hideArrowForward:true},
                 {idKey:"arrive_time", name:"到港时间", logo:require('../../images/icon_orange.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectArriveTime")},
                 {idKey:"last_goods",name:"上载货品", logo:require('../../images/icon_green.png'), disable:false, hideArrowForward:true},
-
-            ]
-        :
-            [
-                {idKey:"ship_name", name:"选择船只", logo:require('../../images/icon_blue.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectShip")},
-                {idKey:"offer", name:"运价", logo:require('../../images/icon_red.png'), disable:true, numeric:true},
-                {idKey:"arrive_time", name:"到港时间", logo:require('../../images/icon_orange.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectArriveTime")},
-                {idKey:"last_goods",name:"上载货品", logo:require('../../images/icon_green.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectLastGoods")},
-
             ];
+        }
+        else {
+            this.config = this.isGoodsOrder() ?
+                [
+                    {idKey:"ship_name", name:"船舶信息", logo:require('../../images/icon_blue.png'), disable:false, hideArrowForward:true},
+                    {idKey:"offer", name:"运价", logo:require('../../images/icon_red.png'), disable:true},
+                    {idKey:"arrive_time", name:"到港时间", logo:require('../../images/icon_orange.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectArriveTime")},
+                    {idKey:"last_goods",name:"上载货品", logo:require('../../images/icon_green.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectLastGoods")},
+
+                ]
+                :
+                [
+                    {idKey:"ship_name", name:"选择船只", logo:require('../../images/icon_blue.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectShip")},
+                    {idKey:"offer", name:"运价", logo:require('../../images/icon_red.png'), disable:true, numeric:true},
+                    {idKey:"arrive_time", name:"到港时间", logo:require('../../images/icon_orange.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectArriveTime")},
+                    {idKey:"last_goods",name:"上载货品", logo:require('../../images/icon_green.png'), disable:false, onPress:this.cellSelected.bind(this, "SelectLastGoods")},
+
+                ];
+        }
     }
 
     componentDidMount() {
-        if (this.state.priceType === OfferPriceEnum.AgreePrice) {
+        if (this.isAgreePrice() || this.isGoodsOrder()) {
             this.requestData();
         }
     }
@@ -68,28 +78,56 @@ export default class HomeOfferPriceVC extends Component {
 
     requestRecommend = async (isReset) => {
         let {info} = this.state;
-        let data = {book_id: info.book_id};
+        if (stringIsEmpty(info.book_id)) {
+            this.setState({
+                refreshing: false,
+            });
+            this.refToast.show("book_id为空，不能查询默认数据");
+            return;
+        }
 
+        let data = {book_id: info.book_id};
         NetUtil.post(appUrl + 'index.php/Mobile/ship/get_default_ship', data)
             .then(
                 (result)=>{
                     if (result.code === 0) {
                         let data = result.data;
 
-                        let lastGoodsSelectedList = [data.last_goods_name];
-                        let list = lastGoodsSelectedList.map(
-                            (info) => {
-                                return info.goods_name;
-                            }
-                        );
+                        if (this.isAgreePrice()) {
+                            if (objectNotNull(data.last_goods_name)) {
+                                let lastGoodsSelectedList = [data.last_goods_name];
+                                let list = lastGoodsSelectedList.map(
+                                    (info) => {
+                                        return info.goods_name;
+                                    }
+                                );
 
-                        this.setState({
-                            ship: data,
-                            offer: data.price,
-                            last_goods: list.join(','),
-                            lastGoodsSelectedList: lastGoodsSelectedList,
-                            refreshing: false,
-                        })
+                                this.setState({
+                                    ship: data,
+                                    offer: data.price,
+                                    last_goods: list.join(','),
+                                    lastGoodsSelectedList: lastGoodsSelectedList,
+                                    refreshing: false,
+                                });
+                            }
+                            else {
+                                this.setState({
+                                    refreshing: false,
+                                });
+                                this.refToast.show("数据出错");
+                            }
+                        }
+                        else if (this.isGoodsOrder()) {
+                            this.setState({
+                                ship: data,
+                                refreshing: false,
+                            });
+                        }
+                        else {
+                            this.setState({
+                                refreshing: false,
+                            });
+                        }
                     }
                     else {
                         this.setState({
@@ -109,12 +147,18 @@ export default class HomeOfferPriceVC extends Component {
         return this.state.priceType === OfferPriceEnum.AgreePrice;
     }
 
-    toGotoTwicePriceVC = (book_id) =>{
-        this.state.info.book_id = book_id;
+    isGoodsOrder() {
+        return this.state.type === OfferOrderEnum.GoodsOrder;
+    }
+
+    toGotoTwicePriceVC = (data) =>{
+        //to fix 报价后book_id和info.book_id不一致
+        if (!objectNotNull(this.state.info.book_id)) {
+            this.state.info.book_id = data.book_id;
+        }
+        // this.refToast.show(this.state.info.book_id + "****" + data.book_id);
         appSecondPriceParams = {headerTitle: "二次报价", info: this.state.info};
         this.props.navigation.goBack('HomeOfferTwicePrice');
-        // this.props.navigation.goBack('Main');
-        // this.props.navigation.navigate('HomeOfferTwicePrice', {info: this.state.info});
     };
 
     onSubmitBtnAction = () => {
@@ -157,7 +201,7 @@ export default class HomeOfferPriceVC extends Component {
                     (result)=>{
                         if (result.code === 0) {
                             PublicAlert(result.message, "",
-                                [{text:"确定", onPress:this.toGotoTwicePriceVC.bind(this, result.data.book_id)}]
+                                [{text:"确定", onPress:this.toGotoTwicePriceVC.bind(this, result.data)}]
                             );
                         }
                         else {
@@ -348,11 +392,10 @@ export default class HomeOfferPriceVC extends Component {
         const { navigate } = this.props.navigation;
         let {info} = this.state;
         let price = parseInt(info.price);
-        let isAgreePrice = this.isAgreePrice();
         return (
             <View style={appStyles.container}>
                 <ScrollView style={{flex: 1, backgroundColor:'#fff'}}
-                            refreshControl={isAgreePrice ?
+                            refreshControl={(this.isAgreePrice() || this.isGoodsOrder()) ?
                                 <RefreshControl
                                     refreshing={this.state.refreshing}
                                     onRefresh={this.requestData.bind(this)}
