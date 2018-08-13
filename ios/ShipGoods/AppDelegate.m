c4128ae9fb17d90fbee5f4ad7e1de693324ae68e
yangqingluo <84807330@qq.com>
2018-08-13 21:14:17 +0800

    ios开发账号切换bug解决

--- ios/ShipGoods/AppDelegate.m
+++ ios/ShipGoods/AppDelegate.m
@@ -17,15 +17,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ShipGoods"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   PublicViewController *rootViewController = [PublicViewController new];
   rootViewController.view = rootView;
